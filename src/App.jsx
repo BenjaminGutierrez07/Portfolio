fa240de8aae9de271dc3865ebeae0d857d5dd6fd
Benjamin Gutierrez <benjamingutierrez0097@gmail.com>
2023-07-28 02:15:09 -0400

    I finish my page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <nav className="nav">
+        <ul className="nav_links">
+          <li>
+          <a className="nav_link" href="https://candid-biscochitos-30d0ef.netlify.app/">About</a>
+          </li>
+          <li>
+            <a className="nav_link" href="https://statuesque-kitsune-e5a07e.netlify.app/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+  
+      <header className="header"> 
+        <h1>Hey I am Benjamin Gutierrez</h1>
+        <p>a web developer</p>
+      </header>
+  
+      <main className="main">
+        <h2>These are my projects</h2>
+        <section className="cards">
+          <div className="card">
+              <a href="https://cheery-monstera-317c66.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <div className="cardOne">
+                  <img src="chees.png" alt="Cheescake" />
+                  <div className='titleImg'>Cheesecake Recipe </div>
+                </div>
+              </a>
+            <a href="https://spontaneous-snickerdoodle-125e51.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <div className="cardTwo">
+                  <img src="calculator.png" alt="Calculadora Restaurante" />
+                  <div className='titleImgTwo'>Restaurant Calculator</div>
+                </div>
+              </a>
+              <a href="https://deluxe-dolphin-0df9ec.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <div className="cardThree">
+                  <img src="paginaweb.png" alt="PaginaWeb" />
+                  <div className='titleImgTh'>Dream's Store</div>
+                </div>
+              </a>
+              <a href="https://benjamingutierrez07.github.io/tictac/" target="_blank" rel="noopener noreferrer">
+                <div className="cardFour">
+                  <img src="tictoctoe.png" alt="tictactoe" />
+                  <div className='titleImgFo'>Tic Tac Toe</div>
+                </div>
+              </a>
+              <a href="https://benjamingutierrez07.github.io/Mini_project/" target="_blank" rel="noopener noreferrer">
+                <div className="cardFive">
+                  <img src="windbnb.png" alt="windbnb" />
+                  <div className='titleImgFi'>Windbnb</div>
+                </div>
+              </a>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
 
